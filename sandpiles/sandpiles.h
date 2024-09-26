b3dbb2ba42c17377daeef5f0d043f71177a86db4
Teheremiti <t.tuiaiho@gmail.com>
2024-09-26 10:30:06 +0200

    WTFFFFF

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,3 +1,6 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -5,3 +8,5 @@ void print_sandpile(int[3][3]);
 int is_stable(int[3][3]);
 void stabilize_sandpile(int[3][3], int[3][3]);
 void sandpiles_sum(int[3][3], int[3][3]);
+
+#endif
