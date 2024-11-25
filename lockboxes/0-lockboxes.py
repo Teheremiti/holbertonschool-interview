c412b51fd7c27c79806dc53bf363b82cdab5d64e
Teheremiti <t.tuiaiho@gmail.com>
2024-11-25 10:50:49 +0100

    Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+def canUnlockAll(boxes):
+    """
+    There are n locked boxes, each numbered sequentially from 0 to n - 1.
+    Each box may contain keys to the other boxes.
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (list[list[int]]): Exactly what it sounds like.
+
+    Returns:
+        boolean: True if all boxes can be opened, otherwise False.
+    """
+    keys = set(boxes[0])
+    for i in range(1, len(boxes)):
+        keys |= set(boxes[i]).union(*(set(boxes[key]) for key in boxes[i]))
+        if i not in keys:
+            return False
+
+    return True
